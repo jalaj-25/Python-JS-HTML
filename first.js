50fa4939325de786426193e1327f374d103b5577
Jalaj Singhal <115914764+jalaj-25@users.noreply.github.com>
2023-12-22 09:21:36 +0530

    Add files via upload

--- first.js
+++ first.js
@@ -1,9 +1,17 @@
+let hour = 12;
+let minute = 30;
+
+if (hour == 12 && minute == 30) {
+  alert( 'The time is 12:30' );
+}
+
+/*
 let hour = 9;
 
 if (hour < 10 || hour > 18) {
   alert( 'The office is closed.' );
 }
-/*
+
 let i = 1;
 do{
     console.log("I = ", i);
