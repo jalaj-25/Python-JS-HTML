64bcfda69628df59bea8d96a4656a58e0b410de6
Jalaj Singhal <115914764+jalaj-25@users.noreply.github.com>
2024-01-09 10:58:00 +0530

    Update first.js

--- javascript/first.js
+++ javascript/first.js
@@ -1,5 +1,75 @@
+let height = null;
+let width = null;
+
+// important: use parentheses
+let area = (height ?? 100) * (width ?? 50);
+
+alert(area); // 5000
+
+/*let str = "123";
+alert(typeof str); // string
+
+let num = Number(str); // becomes a number 123
+
+alert(typeof num); // number
+
+result = prompt(title, [default]);
+
+//Arrow function
+let sayHi = () => alert("Hello!");
+sayHi();
+
+let age = prompt("What is your age?", 18);
+
+let welcome = (age < 18) ?
+  () => alert('Hello!') :
+  () => alert("Greetings!");
+
+welcome();
+ 
+let firstName = null;
+let lastName = null;
+let nickName = "Supercoder";
+
+// shows the first defined value:
+alert(firstName ?? lastName ?? nickName ?? "Anonymous"); // Supercoder
+
+let height = null;
+let width = null;
+
+// important: use parentheses
+let area = (height ?? 100) * (width ?? 50);
+
+alert(area); // 5000
+
+let hour = 12;
+let minute = 30;
+
+if (hour == 12 && minute == 30) {
+  alert( 'The time is 12:30' );
+}
+
+let hour = 12;
+let minute = 30;
+
+if (hour == 12 && minute == 30) {
+  alert( 'The time is 12:30' );
+}
+
+let hour = 9;
+
+if (hour < 10 || hour > 18) {
+  alert( 'The office is closed.' );
+}
+
 let i = 1;
 do{
     console.log("I = ", i);
     i++;
-} while(i < 6);
+} while(i < 6);*/
+
+/*let i = 1;
+do{
+    console.log("I = ", i);
+    i++;
+} while(i < 6);*/
