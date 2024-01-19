8770a8adb304c69d07aa8cafe540c0615295db80
Jalaj Singhal <115914764+jalaj-25@users.noreply.github.com>
2024-01-19 12:21:22 +0530

    Add files via upload

---
+++ Python/arithmatic.py
@@ -0,0 +1,7 @@
+num1 = 50
+num2 = 25
+print("num1 + num2 = ", num1 + num2)
+print("num1 - num2 = ", num1 - num2)
+print("num1 * num2 = ", num1 * num2)
+print("num1 / num2 = ", num1 / num2)
+print("num1 % num2 = ", num1 % num2)
