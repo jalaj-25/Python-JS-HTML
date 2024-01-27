83ee8273c87b4f8106b5fdff9870f861e43209a0
Jalaj Singhal <115914764+jalaj-25@users.noreply.github.com>
2024-01-27 22:46:35 +0530

    Add files via upload

---
+++ Python/Membership opearator.py
@@ -0,0 +1,44 @@
+#str = 'Jaspreet'
+#substr = 'preet'
+#print(substr in str)
+
+print(15 + 3 / 4 * 10 ** 2 ** 2)
+
+#x = 5
+#number = {1,2,3,4,5,6,7,8,9,10}
+#print(x not in number)
+#print(x in number)
+
+#dict = {1: 'Jaspreet', 2: 'Singh'}
+#print('Jaspreet' in dict)
+
+#x = 10
+#y = 10
+#print(x is y)
+#print(x is not y)
+#print(id(x))
+#print(y is 140731365669592)
+
+#x = 5
+#y = 7.5
+#res = x + int(y)#
+#print(res)
+#res = x + y
+#print(res)
+#res = x * y
+#print(res)
+#res = x * int(y)
+#print(res)
+#res = x // y
+#print(res)
+#res = x // int(y)
+#print(res)
+#res = x / y
+#print(res)
+#res = x / int(y)
+#print(res)
+
+#x = 10
+#y = 15.0
+#res = float(x) + y
+#print(res)
