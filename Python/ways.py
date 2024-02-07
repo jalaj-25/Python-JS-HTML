8e350004929ab0b3715c57aec977d346105e5484
Jalaj Singhal <115914764+jalaj-25@users.noreply.github.com>
2024-02-07 21:32:56 +0530

    Add files via upload

---
+++ Python/ways.py
@@ -0,0 +1,8 @@
+str ="welcoe to divyanshu world"
+print("upper case",str.upper())
+print("lower case",str.lower())
+print("split case",str.split())
+print("join case",'-'.join(str.split()))
+print("replace-",str.replace("divyanshu","java++"))
+print("count of o",str.count('o'))
+print("find of -",str.find("to"))
