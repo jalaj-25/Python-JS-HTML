ddc06fbc9f1d63971e2cb7bd10b2f6980da9c11a
Jalaj Singhal <115914764+jalaj-25@users.noreply.github.com>
2024-02-22 23:29:17 +0530

    Add files via upload

--- Python/charcarter.py
+++ Python/charcarter.py
@@ -1,7 +1,38 @@
+import re
 txt = "The rain in spain!"
-x = re.findall(r"\Bain",txt)
+#x = re.findall("\ATHE",txt)
+#x = re.findall(r"ain\b",txt)
+#x = re.findall("\d",txt)
+#x = re.findall("\D",txt)
+#x = re.findall("\s",txt)
+#x = re.findall("\w", txt)
+#x = re.findall("\W", txt)
+x = re.findall("Spain\Z", txt)
+
 print(x)
 if x:
-    print("Yes it match!")
+    print("Yes,at least one match!")
 else:
     print("NO MATCH")
+
+pattern = r"[a-zA-Z]+ \d+"
+matches = re.findall(pattern, "LXI 2013, VXI 2015, VDI 2014, MARUTI SUZUKI CARS I N INDIA")
+for match in matches:
+    print(match, end=" ")
+
+import re
+string = "she sells sea shell on the sea shore"
+pattern = "sea"
+repl = "ocean"
+new_string = re.sub(pattern, repl, string, 2)
+print(new_string)
+
+var = "Good"
+def show():
+    global var1
+    var1 = "Morning"
+    print("The function var is: ", var)
+show()
+print("Outside funciton var is: ", var1)
+print("Var is: ", var)
+
---
+++ Python/cube.py
@@ -0,0 +1,34 @@
+"""cubr of nuber using function
+def cube(x):
+    return (x*x*x)
+num = 13
+result = cube(num)
+print('Cube of ', num, ' is: ', result) 
+"""
+
+"""function for multipluication of number using function
+def func(x):
+    return 5 * x
+print(func(3))
+print(func(4))
+print(func(5))"""
+
+
+"""No argument is present
+Take exactly one argument
+def display(str):
+    print(str)
+str="HELLO"
+'''print("HELLO")'''
+display(str)"""
+
+
+"""def display(str, int_x, float_y):
+    print("The string is: ", str)
+    print("The integer value is: ", int_x)
+    print("Float value is ", float_y)
+display(float_y = 5698.78954, str="hello", int_x = 123654)"""
+
+def func(child3,  child2, child1):
+    print("The youngest childf is: ", child3)
+func(child1 = "Fail", child2 = "tobais", child3 = "Lions")
