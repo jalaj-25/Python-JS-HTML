d0a21b5afaf9af783ed0c8d20599230e351648d3
Jalaj Singhal <115914764+jalaj-25@users.noreply.github.com>
2024-06-15 21:53:09 +0530

    Add files via upload

---
+++ chat/script.js
@@ -0,0 +1,19 @@
+const artists = [
+    {name: 'Arijit Singh', id: 1},
+    {name: 'KK', id: 1},
+    {name: 'VILEN', id: 1},
+    {name: 'Honey Singh', id: 1},
+];
+
+const musicList = [
+    {title: 'Sajni', artistId: 1, url:'https://open.spotify.com/embed/track/4gU4FIc5N5nqg6W29LwXIs?si=83b4f693b90341aa'},
+    {title: 'Satranga', artistId: 1, url:'https://open.spotify.com/embed/track/3yHyiUDJdz02FZ6jfUbsmY?si=2b5ab91f4f474717'},
+    {title: 'Heeriye', artistId: 1, url:'https://open.spotify.com/embed/track/5PUXKVVVQ74C3gl5vKy9Li?si=d9cbb0f6eed84d3c'},
+    {title: 'Dil Jhoom', artistId: 1, url:'https://open.spotify.com/embed/track/11qnwt18ZckPqchzK2I3U7?si=1b3e06e04b3b4400'},
+    {title: 'Chaleya', artistId: 1, url:'https://open.spotify.com/embed/track/4nc6XiUze2Yh7wFueGOPv7?si=68f51445d5a54509'},
+]
+
+const artistcontainer = document.getElementById('artists');
+const musicListContainer = document.getElementById('music-list');
+const playerContainer = document.getElementById('player');
+const searchInput = document.getElementById('search');
